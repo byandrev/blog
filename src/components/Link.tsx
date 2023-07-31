bbfc14b9a34438c8b5546b3dd8c134df3702d725
Andres Parra <byandrev@gmail.com>
2023-07-30 21:14:11 -0500

    add as, className props to Link component

--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -3,13 +3,20 @@ import LinkNext from "next/link";
 
 type LinkProps = {
   children: ReactNode;
+  as?: string;
   href: string;
   target?: "_blank" | "_self" | "_parent" | "_top";
+  className?: string;
 };
 
-const Link: FC<LinkProps> = ({ children, href, target }) => {
+const Link: FC<LinkProps> = ({ children, href, target, as, className }) => {
   return (
-    <LinkNext className="text-primary hover:underline" href={href} target={target}>
+    <LinkNext
+      className={`text-primary hover:underline ${className}`}
+      as={as}
+      href={href}
+      target={target}
+    >
       {children}
     </LinkNext>
   );
