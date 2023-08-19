f5fc05a62bfeee89e581adb967580418de361f72
Andres Parra <byandrev@gmail.com>
2023-08-19 12:02:14 -0500

    add social links

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,6 +3,7 @@
 import { useTranslation } from "@/app/i18n/client";
 import Container from "@/components/Container";
 import Link from "@/components/Link";
+import { userLinks } from "@/config";
 
 function Footer({ lang }: { lang: string }) {
   const { t } = useTranslation(lang);
@@ -13,11 +14,15 @@ function Footer({ lang }: { lang: string }) {
         <div className="flex justify-between">
           <p>{t("footer.text")}</p>
 
-          <p>
-            <Link href="https://github.com/byandrev/blog" target="_blank">
-              Github
-            </Link>
-          </p>
+          <div className="flex gap-4">
+            {userLinks.map((link) => (
+              <p key={link.text}>
+                <Link href={link.url} target="_blank">
+                  {link.text}
+                </Link>
+              </p>
+            ))}
+          </div>
         </div>
       </Container>
     </footer>
---
+++ src/config/index.ts
@@ -0,0 +1,19 @@
+type Link = {
+  text: string;
+  url: string;
+};
+
+export const userLinks: Link[] = [
+  {
+    text: "Github",
+    url: "https://github.com/byandrev",
+  },
+  {
+    text: "Twitter",
+    url: "https://twitter.com/byandrev",
+  },
+  {
+    text: "Instagram",
+    url: "https://instagram.com/byandrev",
+  },
+];
